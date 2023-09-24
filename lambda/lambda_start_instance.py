23bedb5fd961ed77c9ee04917370ef78cc5ceb1f
Krishna-kanth95 <93731192+Krishna-kanth95@users.noreply.github.com>
2023-09-23 21:27:58 -0400

    Update lambda_start_instance.py

--- lambda/lambda_start_instance.py
+++ lambda/lambda_start_instance.py
@@ -1,4 +1,5 @@
 import json
+import time
 import boto3
 
 ec2 = boto3.client('ec2')
@@ -15,11 +16,17 @@ def lambda_handler(event, context):
     
     state_name = instance_state(instance_id)
 
-    if state_name == 'stopped' or state_name == 'stopping':
-        if state_name == 'stopped':
-            ec2.start_instances(InstanceIds=[instance_id])
-            print(f'{instance_id} server is starting up.')
-        else:
-            print(f'{instance_id} server is currently stopping. Cannot start until fully stopped.')
+    if state_name == 'stopped':
+        ec2.start_instances(InstanceIds=[instance_id])
+        print(f'{instance_id} server is starting up.')
+    
+    elif state_name == 'stopping':
+        print(f'{instance_id} server is currently stopping. Cannot start until fully stopped.')
+        
+        while instance_state(instance_id) != 'stopped':
+            time.sleep(10)
+        ec2.start_instances(InstanceIds=[instance_id])
+        print(f'{instance_id} server is starting up.')
+
     else:
         print(f'Nothing to do. Current state is {state_name}.')
