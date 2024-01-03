50430f24d9801a84a9d01100e6b5e1de18d3fdbf
Krishna-kanth95 <93731192+Krishna-kanth95@users.noreply.github.com>
2024-01-03 17:34:16 -0500

    Update lambda_new_server_automation.py

--- lambda/lambda_new_server_automation.py
+++ lambda/lambda_new_server_automation.py
@@ -4,7 +4,7 @@ import boto3
 import requests
 import base64
 
-BUCKET_NAME = 'testlambda1bucket'
+BUCKET_NAME = 'testlambda1bucket11'
 INSTANCE_FILE_KEY = 'instance-ips.txt'
 GITHUB_INVENTORY = 'https://api.github.com/repos/Krishna-kanth95/ghar/contents/inventory.ini'
 GITHUB_ACTIONS = 'https://api.github.com/repos/Krishna-kanth95/ghar/actions/workflows/ghar.yml/dispatches'
@@ -14,37 +14,32 @@ headers['Authorization'] = f'Bearer {os.environ["GIT_TOKEN"]}'
 headers['Accept'] = 'application/vnd.github.v3+json'
 headers['X-GitHub-Api-Version'] = '2022-11-28'
 
-# def encode_ips(private_ip):
-#     encoded_ips = []
-#     for ip in private_ip:
-#         encoded_ip = base64.b64encode(ip.encode('utf-8')).decode('utf-8')
-#         encoded_ips.append(encoded_ip)
-#     return encoded_ips
-
-
-def update_git_inventory_file(private_ips):
+def update_git_inventory_file(server_details):
     try:
         git_sha = requests.get(GITHUB_INVENTORY, headers=headers)
-        git_sha.raise_for_status()  # Raise an exception for non-2xx status codes
+        git_sha.raise_for_status()
     except Exception as e:
         print(f"SHA Get request update failed: {e}")
         raise
 
     sha = json.loads(git_sha.text)["sha"]
-    print(sha)
 
-    # Replace existing private IPs with new private IPs
-    new_content = '[servers]\n' + '\n'.join(private_ips)
+    inventory_lines = ['[servers]']
+    for details in server_details:
+        name, ip = details.split(':')
+        inventory_lines.append(f'{name} ansible_host={ip}')
+
+    new_content = '\n'.join(inventory_lines)
 
     data = {
         "sha": sha,
-        "message": "Updating inventory with new private IPs",
+        "message": "Updating inventory with new private IPs and server names",
         "content": base64.b64encode(new_content.encode('utf-8')).decode('utf-8')
     }
 
     try:
         update_inv = requests.put(GITHUB_INVENTORY, headers=headers, json=data)
-        update_inv.raise_for_status()  # Raise an exception for non-2xx status codes
+        update_inv.raise_for_status()
     except Exception as e:
         print(f"Inventory file update failed: {e}")
         raise
@@ -52,14 +47,13 @@ def update_git_inventory_file(private_ips):
     print(update_inv.text)
     return True
 
-
 def trigger_github_actions(repo, token):
     data2 = {
         "ref": "main"
     }
 
     ghar = requests.post(GITHUB_ACTIONS, headers=headers, json=data2)
-    ghar.raise_for_status()  # Raise an exception for non-2xx status codes
+    ghar.raise_for_status()
     print(ghar.text)
 
 def lambda_handler(event, context):
@@ -67,44 +61,39 @@ def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name=region)
     s3 = boto3.client('s3')
     
-    # Read known private IPs from S3 bucket
+    # Retrieve the list of known servers from the S3 bucket
     response = s3.get_object(Bucket=BUCKET_NAME, Key=INSTANCE_FILE_KEY)
-    known_private_ips = set(ip.strip() for ip in response['Body'].read().decode('utf-8').split('\n') if ip.strip())
+    known_servers = set(response['Body'].read().decode('utf-8').split('\n'))
 
-    # Get information about all instances
+    # Obtain current servers from EC2 instances
     ec2_response = ec2.describe_instances()
-
-    # Initialize set to store current private IP addresses
-    current_private_ips = set()
-
+    current_servers = set()
+    
     for reservation in ec2_response['Reservations']:
         for instance in reservation['Instances']:
-            # Check if 'PrivateIpAddress' key exists
             if 'PrivateIpAddress' in instance:
-                current_private_ips.add(instance['PrivateIpAddress'])
+                ip_address = instance['PrivateIpAddress']
+                instance_name = next((tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'), 'Unknown')
+                current_servers.add(f'{instance_name}:{ip_address}')
 
+    new_servers = current_servers - known_servers
 
-    # Find new private IPs
-    new_private_ips = current_private_ips - known_private_ips
-
-    if new_private_ips:
-        # Update Git inventory file with new private IP addresses
-        is_update_successful = update_git_inventory_file(list(new_private_ips))
+    if new_servers:
+        is_update_successful = update_git_inventory_file(list(new_servers))
 
         if is_update_successful:
-            # Trigger GitHub Actions workflow only if the update was successful and there are new instances
             trigger_github_actions(GITHUB_ACTIONS, os.environ["GIT_TOKEN"])
 
-            # Update known_private_ips and write to S3 only if there are new private IPs
-            known_private_ips.update(new_private_ips)
-            s3.put_object(Body='\n'.join(known_private_ips), Bucket=BUCKET_NAME, Key=INSTANCE_FILE_KEY)
+            known_servers.update(new_servers)
+            updated_servers_content = '\n'.join(known_servers)
+            s3.put_object(Body=updated_servers_content, Bucket=BUCKET_NAME, Key=INSTANCE_FILE_KEY)
 
             return {
                 'statusCode': 200,
-                'body': json.dumps(f'New instances with IPs {new_private_ips} added and GitHub Actions triggered.')
+                'body': json.dumps(f'New servers {new_servers} added, GitHub Actions triggered, and S3 updated.')
             }
     else:
         return {
             'statusCode': 200,
-            'body': json.dumps('No new instances; no action taken.')
+            'body': json.dumps('No new servers; no action taken.')
         }
